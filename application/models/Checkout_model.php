966d9686fac8c239770a310e51f47c75da43e603
alfianrenz <alfianrenz25@gmail.com>
2020-07-29 14:24:35 +0700

    terbaru

--- application/models/Checkout_model.php
+++ application/models/Checkout_model.php
@@ -8,11 +8,20 @@ class Checkout_model extends CI_Model
     //get data detail keranjang
     public function getdetail_keranjang()
     {
+        // $this->db->select('*');
+        // $this->db->from('detail_keranjang');
+        // $this->db->join('keranjang', 'detail_keranjang.id_keranjang = keranjang.id_keranjang');
+        // $this->db->join('produk', 'detail_keranjang.id_produk = produk.id_produk');
+        // $this->db->where('id_pembeli', $this->session->userdata('id'));
+        // return $this->db->get()->result_array();
+
         $this->db->select('*');
-        $this->db->from('detail_keranjang');
-        $this->db->join('keranjang', 'detail_keranjang.id_keranjang = keranjang.id_keranjang');
+        $this->db->from('keranjang');
+        $this->db->join('detail_keranjang', 'keranjang.id_keranjang = detail_keranjang.id_keranjang');
         $this->db->join('produk', 'detail_keranjang.id_produk = produk.id_produk');
-        $this->db->where('id_pembeli', $this->session->userdata('id'));
+        $this->db->where('keranjang.id_pembeli', $this->session->userdata('id'));
+        $this->db->where('keranjang.tipe_pembeli', $this->session->userdata('tipe'));
+        $this->db->where('keranjang.status_keranjang', 0);
         return $this->db->get()->result_array();
     }
 
