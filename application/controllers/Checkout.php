e3538252fe7e37db843c01ac271290da381cdd41
alfianrenz <alfianrenz25@gmail.com>
2020-07-31 21:00:23 +0700

    terbaru

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -124,39 +124,12 @@ class Checkout extends My_Controller
 
     public function redirect()
     {
-        // $data = json_decode(file_get_contents('php://input'));
         $order_id = $this->input->get('order_id');
+        $data['transaksi'] = $this->db->get_where('transaksi', ['order_id' => $order_id])->row();
 
-        //transaksi
-        $transaksi = $this->db->select('*')
-            ->from('transaksi')
-            ->join('detail_keranjang', 'transaksi.id_keranjang = detail_keranjang.id_keranjang')
-            ->join('produk', 'detail_keranjang.id_produk = produk.id_produk')
-            ->join('akun_mahasiswa', 'produk.id_mahasiswa = akun_mahasiswa.id_mahasiswa')
-            ->where('transaksi.order_id', $order_id)
-            ->get()->row();
-
-
-        //item
-        $item = $this->db->select('*')
-            ->from('detail_keranjang')
-            ->join('produk', 'detail_keranjang.id_produk = produk.id_produk')
-            ->join('akun_mahasiswa', 'produk.id_mahasiswa = akun_mahasiswa.id_mahasiswa')
-            ->join('mahasiswa', 'akun_mahasiswa.nim = mahasiswa.nim')
-            ->join('transaksi', 'detail_keranjang.id_keranjang = transaksi.id_keranjang')
-            ->where('transaksi.id_pembeli')
-            ->where('transaksi.order_id', $order_id)
-            ->get()->result_array();
-
-
-        // $data['transaksi'] = $this->db->get_where('transaksi', ['order_id' => $order_id])->row();
-        $data['transaksi'] = $transaksi;
-        $data['item'] = $item;
-
-        // $id = $this->db->get('keranjang')->row_array();
         $this->db->where('id_keranjang', $data['transaksi']->id_keranjang);
         $this->db->update('keranjang', ['status_keranjang' => 1]);
 
-        $this->paggingFrontend('frontend/invoice', $data);
+        $this->paggingFrontend('frontend/redirect', $data);
     }
 }
